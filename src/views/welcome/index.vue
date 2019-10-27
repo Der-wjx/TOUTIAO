179045ba617199159082deb0c8f734b24d4e7a4e
Der-wjx <1797707514@qq.com>
2019-10-27 21:52:58 +0800

    首页初始化完成

---
+++ src/views/welcome/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+      <img src="../../assets/welcome.jpg" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.container {
+    img {
+        display: block;
+        margin: 0 auto;
+    }
+}
+</style>
